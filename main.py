857d79a4b6fd208768f247cbbd13d2a4b4bddabc
Zynh0722 <Zynh0722@gmail.com>
2022-11-17 20:46:58 -0800

    more work

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from dotenv import dotenv_values
+from networktables import NetworkTables
+import cv2 as cv2
+import argparse
+import apriltag
 
 ip = dotenv_values(".env")["RIO_IP"]
+
+if ip == "10.TE.AM.2":
+    print("Error: specify an IP to connect to!")
+    exit(0)
+
+NetworkTables.initialize(server=ip)
+sd = NetworkTables.getTable("SmartDashboard") # Use the smart dashboard table
+
+# Get argparser for apriltag lib
+parser = argparse.ArgumentParser(description='Detect AprilTags from video stream.')
+apriltag.add_arguments(parser)
+options = parser.parse_args()
