1b7cf0ab116061d33bd0f0750cd15c883ce954c5
Zynh0722 <Zynh0722@gmail.com>
2022-11-19 16:17:59 -0800

    more code

--- helpers.py
+++ helpers.py
@@ -7,9 +7,10 @@ def round_iter(iterable):
 
 
 env = dotenv_values(".env")
+LINE_LENGTH = 5
 TEXT_COLOR = round_iter(env["TEXT_COLOR"].split(','))
 LINE_COLOR = round_iter(env["LINE_COLOR"].split(','))
-NORMAL_COLOR = env["NORMAL_COLOR"].split(',')
+CENTER_COLOR = round_iter(env["CENTER_COLOR"].split(','))
 
 
 def get_turn(center):
@@ -22,14 +23,64 @@ def clamp_min_abs(value, min_abs):
     return value if abs(value) > min_abs else 0
 
 
-def plot_quad(old_image, quad):
+def plot_quad(old_image, quad, alt_color=False):
     image = old_image
+
+    color = CENTER_COLOR if alt_color else LINE_COLOR
 
     for edge in zip(quad[::], (*quad[1::], quad[0])):
         point_1, point_2 = edge
         image = cv2.line(image, 
                          round_iter(point_1),
                          round_iter(point_2),
-                         LINE_COLOR, 3)
+                         color, 3)
 
     return image
+
+
+def plot_point(old_image, center):
+    image = old_image
+
+    center = (int(center[0]), int(center[1]))
+    image = cv2.line(image,
+                     (center[0] - LINE_LENGTH, center[1]),
+                     (center[0] + LINE_LENGTH, center[1]),
+                     CENTER_COLOR,
+                     3)
+    image = cv2.line(image,
+                     (center[0], center[1] - LINE_LENGTH),
+                     (center[0], center[1] + LINE_LENGTH),
+                     CENTER_COLOR,
+                     3)
+    return image
+
+
+def plot_auto_state(image, auto_state):
+    return cv2.putText(image, 
+                       f"Autonomous: {'Enabled' if auto_state else 'Disabled'}",
+                       (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+
+def plot_detecting(image, always_active, auto_state):
+    text = "Enabled" if always_active or auto_state else "Disabled"
+    if always_active:
+        text += " -A"
+
+    return cv2.putText(image, 
+                    f"Detection: {text}",
+                    (10, 65), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+
+def plot_text(image, center, text):
+    center = (int(center[0]) + 4, int(center[1]) - 4)
+    return cv2.putText(image, str(text), center, cv2.FONT_HERSHEY_SIMPLEX,
+                       1, CENTER_COLOR, 3)
+
+
+def nt_drive(sd, fwd, rot):
+    sd.putNumber('drive_scalar', fwd)
+    sd.putNumber('turn_scalar', rot)
+
+
+def first(iterable, condition = lambda x: True):
+    return next(x for x in iterable if condition(x))
--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import cv2 as cv2
 import argparse
 import apriltag
 
-from helpers import get_turn, plot_quad
+from helpers import (
+    get_turn,
+    plot_quad,
+    plot_point,
+    plot_auto_state,
+    plot_detecting,
+    plot_text,
+    nt_drive,
+    first
+)
 
 env = dotenv_values(".env")
 ip = env["RIO_IP"]
+drive_tag_id = int(env["DRIVE_TAG_ID"])
 
 # Quick check that you actuall set the ip <3
 if ip == "10.TE.AM.2":
@@ -36,16 +46,33 @@ loop = True
 while loop:
     _, image = cam.read()
 
-    if options.always_active or sd.getBoolean('auto_state', False):
+    auto_state = sd.getBoolean('auto_state', False)
+    detecting = options.always_active or auto_state
+
+    image = plot_auto_state(image, auto_state)
+    image = plot_detecting(image, options.always_active, auto_state)
+
+    if detecting:
         grayimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         detections = detector.detect(grayimg)
-    
-        for detection in detections:
-            image = plot_quad(image, detection.corners)
+
+        if detections:
+            for detection in detections:
+                image = plot_quad(
+                    image, detection.corners, detection.tag_id == drive_tag_id)
+                image = plot_text(image, detection.center, detection.tag_id)
+                image = plot_point(image, detection.center)
+
+            drive_tag = first(detections, lambda tag: tag.tag_id == drive_tag_id)
+
+            if drive_tag:
+                nt_drive(sd, 0.2, get_turn(drive_tag.center))
+        else:
+            nt_drive(sd, 0, 0)
 
     cv2.imshow('Camera View', image)
 
-    key = cv2.waitKey(10)
+    key = cv2.waitKey(1)
     if key == 13:
         loop = False
 
